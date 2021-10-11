fb260e5c536f5bc2f7972578f0ca2d26711500a5
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-10-11 08:51:00 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,21 @@ module.exports = {
     'linebreak-style': 0,
     'max-len': [1, 600, 2],
     'no-plusplus': [2, { allowForLoopAfterthoughts: true }],
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id'],
+        },
+      },
+    ],
+    'jsx-a11y/label-has-for': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id'],
+        },
+      },
+    ],
   },
 };
